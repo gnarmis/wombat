10dff2313f655c0c9b8c3a6fca9faae32b47016d
Gursimran Singh <g13singh@gmail.com>
2013-06-19 01:58:57 -0500

    PROGRESS: spitting html into index file

--- src/wombat/core.clj
+++ src/wombat/core.clj
@@ -1,2 +1,17 @@
-(ns wombat.core)
+(ns wombat.core
+  (:require [hiccup.core :as h]
+            [clojure.java.io :as io]))
 
+(def target
+  "Describes the output file locations"
+  {:index "public/index.html"})
+
+(def eg-html (h/html [:div "hello!"]))
+
+(defn write-index
+  "Spits given string into the index file"
+  [target content]
+  (spit (:index target)
+        content))
+
+(write-index target eg-html)
