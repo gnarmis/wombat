23caf89e6c7b7ebd3e8620b5fb1000c715dd9278
Gursimran Singh <g13singh@gmail.com>
2013-06-26 21:12:26 -0500

    cleaned up, added motivations to readme

--- src/wombat/styles/layout.clj
+++
@@ -1,7 +0,0 @@
-(ns wombat.styles.layout
-  (:require [garden.core :refer [css]]
-            [garden.stylesheet :refer [at-media]]))
-
-(def layout-rules
-  "All rules pertaining to layout"
-  nil)
